38303b28df5b99147c5708308e53e3e43146a136
Jose Pedraza <pastorpedraza3@gmail.com>
2020-10-27 14:07:55 -0600

    set home page

---
+++ src/home.js
@@ -0,0 +1,10 @@
+const home = () => {
+  const container = document.getElementById('container');
+  const div = document.createElement('div');
+  div.setAttribute('class','align-loading');
+  container.appendChild(div);
+  div.innerHTML = '<span>Waiting for input....</span>';
+
+}
+
+export default home;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import getData from './getdata';
+import home from './home'; 
 
-  const searchbttn = document.getElementById('searchbttn');
-  searchbttn.addEventListener('click', search => {
+const searchbttn = document.getElementById('searchbttn');
+searchbttn.addEventListener('click', search => {
   const form = document.getElementsByTagName('form')[0];
   const input = document.getElementById('search');
   const location = input.value;
   getData(location);
   form.reset();
 });
+
+home();
