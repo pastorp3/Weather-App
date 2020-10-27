3e4951a0ccda6eba49b428cf1a7f95b240814cc6
Jose Pedraza <pastorpedraza3@gmail.com>
2020-10-27 13:54:57 -0600

    add Dom manipulation to render.js

---
+++ src/extractinfo.js
@@ -0,0 +1,49 @@
+const city = (input) => {
+	let city = input.name;
+	let country = input.sys.country;
+	let output = city + ', ' + country;
+	return output
+}
+
+const cord = (input) => {
+	let lat = input.coord.lat;
+	let lon = input.coord.lon;
+	let location = 'Latitude ' + lat +', Longitude ' + lon; 
+	return location;
+}
+
+const temp = (input) => {
+	const option = document.getElementById('select-temperature').value;
+	let actual;
+	if(option === 'ºC'){
+		actual = calTempCelcius(input);
+	} else {
+		actual = calTempFah(input)
+	}
+	return actual;
+}
+
+function calTempCelcius(input) {
+	let temp = Math.round(input - 273.15);
+	temp += 'ºC';
+	return temp;
+}
+
+function calTempFah(input) {
+	let temp = Math.round((input - 273.15) * 9/5 + 32);
+	temp += 'ºF';
+	return temp;
+}
+
+const hum = (input) => {
+	let humidity = input.main.humidity + '%';
+	return humidity;
+}
+
+const status = (input) => {
+	let condition = input.weather[0].description;
+	return condition
+}
+
+
+export { city, cord, temp, hum, status };
---
+++ src/render.js
@@ -0,0 +1,77 @@
+import { city, cord, temp, hum, status } from './extractinfo';
+
+function clear(container) {
+  while (container.firstChild) {
+    container.removeChild(container.firstChild);
+  }
+}
+
+
+const render = (img,data) => {
+	const title = city(data);
+	const location = cord(data);
+	const actualTemp = temp(data.main.temp);
+	const feelsLike = temp(data.main.feels_like);
+	const humidity = hum(data);
+	const condition = status(data);
+	const tempMax = temp(data.main.temp_max);
+	const temMin = temp(data.main.temp_min);
+
+	const container = document.getElementById('container');
+	clear(container);
+	const Card = document.createElement('div');
+	const background = document.createElement('div');
+	container.appendChild(Card);
+	Card.appendChild(background);
+	background.innerHTML = `<img src="${img}" class="background" >`;
+	const changebttn = document.getElementById('select-temperature');
+	const dataContainer = document.createElement('div');
+	const titleCity = document.createElement('p');
+	const ubication = document.createElement('p');
+	const tempNow = document.createElement('p');
+	const feelsTemp = document.createElement('p');
+	const Humidity = document.createElement('p');
+	const description = document.createElement('p');
+	const timeDiv = document.createElement('div');
+	const Max = document.createElement('span');
+	const Min = document.createElement('span');
+	Card.appendChild(dataContainer);
+	dataContainer.appendChild(titleCity)
+	dataContainer.appendChild(ubication);
+	dataContainer.appendChild(tempNow);
+	dataContainer.appendChild(feelsTemp);
+	dataContainer.appendChild(Humidity);
+	dataContainer.appendChild(description);
+	dataContainer.appendChild(timeDiv);
+	timeDiv.appendChild(Max);
+	timeDiv.appendChild(Min);
+	dataContainer.setAttribute('class','align-container center');
+	titleCity.setAttribute('class','title');
+	ubication.setAttribute('class','location normal-margin');
+	tempNow.setAttribute('class','tempNow normal-margin');
+	feelsTemp.setAttribute('class','feels_like normal-margin');
+	Humidity.setAttribute('class', 'feels_like normal-margin');
+	description.setAttribute('class','feels_like normal-margin');
+	titleCity.textContent = `${title}`
+	ubication.textContent = `${location}`;
+	tempNow.textContent = `${actualTemp}`;
+	feelsTemp.textContent = `Feels like: ${feelsLike}`;
+	Humidity.textContent = `Humidity: ${humidity}`;
+	description.textContent = `${condition}`;
+	timeDiv.setAttribute('class','flex');
+	Max.setAttribute('class','temp margin-right');
+	Min.setAttribute('class', 'temp');
+	Max.innerHTML = `Max<br>${tempMax}`;
+	Min.innerHTML = `Min<br>${temMin}`;
+
+	changebttn.addEventListener('click', change => {
+		render(img,data);
+	});
+
+
+
+
+
+}
+
+export default render;
