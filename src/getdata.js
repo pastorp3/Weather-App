46211352d679c44bce069f7162fc2cdc4f04fba3
Jose Pedraza <pastorpedraza3@gmail.com>
2020-10-27 13:58:49 -0600

    fix indentation

--- src/getdata.js
+++ src/getdata.js
@@ -1,43 +1,34 @@
- import render from './render';
+import render from './render';
 
- async function getImage(input, info ) {
- 	const url = 'https://api.unsplash.com/photos/random?orientation=landscape&count=1&client_id=7XimdnnMOSmoeVFofk7rEM4_ApTXYE83u8ZAAlOfPZU&query='+input;
- 	const response = await fetch(url);
- 	const data = await  response.json();
- 	render(data[0].urls.regular,info);
- }
+async function getImage(input, info) {
+  const url = 'https://api.unsplash.com/photos/random?orientation=landscape&count=1&client_id=7XimdnnMOSmoeVFofk7rEM4_ApTXYE83u8ZAAlOfPZU&query=' + input;
+  const response = await fetch(url);
+  const data = await response.json();
+  render(data[0].urls.regular, info);
+}
 
-
- async function fetchData(input) {
- 		const url = 'http://api.openweathermap.org/data/2.5/weather?q=' + input + ',&APPID=be7a948e30f210ef85214d3ba4b94273';
-		const response = await fetch(url);
-		const data = await  response.json();
-	return new Promise((resolve, reject) => {
-		
-		if(data.cod === '404') {
-			console.log('Error');
-			reject('No valid city');
-			
-		} else {
-			console.log('Complete');
-			resolve(data);
-
-		}
-	});
-};
-
-
-
+async function fetchData(input) {
+  const url = 'http://api.openweathermap.org/data/2.5/weather?q=' + input + ',&APPID=be7a948e30f210ef85214d3ba4b94273';
+  const response = await fetch(url);
+  const data = await response.json();
+  return new Promise((resolve, reject) => {
+    if (data.cod === '404') {
+      console.log('Error');
+      reject('No valid city');
+    } else {
+      console.log('Complete');
+      resolve(data);
+    }
+  });
+}
 
 async function getData(input) {
-	try {
-		const request = await fetchData(input);
-		getImage(request.weather[0].main, request);
-	} catch(error) {
-		alert(error);
-	}
-	
+  try {
+    const request = await fetchData(input);
+    getImage(request.weather[0].main, request);
+  } catch (error) {
+    alert(error);
+  }
 }
 
 export default getData;
-
--- src/index.js
+++ src/index.js
@@ -1,13 +1,10 @@
 import getData from './getdata';
 
-
-
-const searchbttn = document.getElementById('searchbttn');
-searchbttn.addEventListener('click', search => {
-	const form = document.getElementsByTagName('form')[0];
-	const input = document.getElementById('search');
-	let location = input.value;
-	getData(location);
-	form.reset();
-
+  const searchbttn = document.getElementById('searchbttn');
+  searchbttn.addEventListener('click', search => {
+  const form = document.getElementsByTagName('form')[0];
+  const input = document.getElementById('search');
+  const location = input.value;
+  getData(location);
+  form.reset();
 });
