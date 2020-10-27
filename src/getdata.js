bab0aa424e05456876e2bd72228d766d89242a04
Jose Pedraza <pastorpedraza3@gmail.com>
2020-10-27 14:36:07 -0600

    change api urls

--- src/getdata.js
+++ src/getdata.js
@@ -10,7 +10,7 @@ async function getImage(input, info) {
 }
 
 async function fetchData(input) {
-  const url = 'http://api.openweathermap.org/data/2.5/weather?q=' + input + ',&APPID=be7a948e30f210ef85214d3ba4b94273';
+  const url = 'https://api.openweathermap.org/data/2.5/weather?q=' + input + ',&APPID=be7a948e30f210ef85214d3ba4b94273';
   const response = await fetch(url);
   const data = await response.json();
   return new Promise((resolve, reject) => {
